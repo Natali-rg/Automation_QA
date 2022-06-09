782e63fcc8ca14e2c3b648413530f5c8f8208df2
amenson1983 <amenson1983@gmail.com>
2022-06-09 15:59:16 +0300

    -work with classes. Lesson 5.

--- src/main/java/Lesson5.java
+++ src/main/java/Lesson5.java
@@ -1,2 +1,47 @@
 public class Lesson5 {
+    public static void main(String[] args) {
+
+        Student student=new Student("Turchyna","Natali", 39);
+//        System.out.println(student.age);
+//        student.print();
+//        System.out.println("\n");
+
+        Student student1=new Student("Ivanov", "Ivan", 35);
+//        student1.print();
+//        System.out.println("\n");
+
+        Student student2=new Student("Tarasov","Valeriy",36,122456);
+//        student2.print();
+//        System.out.println("\n");
+
+        Student.str="Animal";
+//        student1.print();
+//        student2.print();
+
+//        System.out.println("\n");
+//        student2.id=2345677;
+//        student2.print();
+
+//        System.out.println("\n");
+//        Student student3=new Student();
+//        student3.print();
+
+
+        /////////encapsulation////////
+        System.out.println(student.getAge());
+        int studentAge= student.getAge();
+        studentAge++;
+
+
+        Eyes eyes = new Eyes("red", 3, true, 2);
+        Cats cats = new Cats(12, "blue", false, 'm',eyes);
+
+
+
+
+
+
+
+
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,71 @@
 public class Student {
+    private String family;
+    private String name;
+    private int age;
+    private int id;
+    private int level;
+
+    static public String str="Human";
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void modifayLewel(int mark) {
+        if ((level<10) && (level>0)){
+           this.level=this.level+mark;
+        }
+    }
+
+    public Student(String family, String name, int age) {
+        int id;
+        this.family = family;
+        this.name = name;
+        this.age = validate(age);
+        this.id=(int) (Math.random()*10000);
+        this.level=0;
+    }
+
+    private int validate(int age) {
+        int result=16;
+        if ((age>15) &&(age<55)){
+            return age;
+        }
+        return 16;
+    }
+
+    public Student(String family, String name, int age, int id) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.id=id;
+        this.level=0;
+    }
+
+    public Student() {
+        System.out.println("constructor Error");
+        this.family="Antonov";
+        this.name="Anton";
+        this.age=16;
+        this.id=2554477;
+        this.level=100;
+    }
+
+
+    public void print(){
+        System.out.println("Family: "+this.family);
+        System.out.println("Name: "+this.name);
+        System.out.println("Age: "+this.age);
+        System.out.println("ID: "+id);
+        System.out.println(str);
+    }
+
+
+
 
 }
